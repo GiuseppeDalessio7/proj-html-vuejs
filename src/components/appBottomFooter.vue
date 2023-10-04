8bfb4567ac487d8d7b7ac14126e8648ad4177c7d
GiuseppeDalessio7 <g.dalessio03gmail.com>
2023-10-04 19:55:06 +0200

    Work to bottomFooter

--- src/components/appBottomFooter.vue
+++ src/components/appBottomFooter.vue
@@ -15,8 +15,20 @@ export default {
 </script>
 
 <template>
-    <div class="orangeb">
+    <div class="orangeb d-flex">
         <img src="../assets/img/news.png" alt="">
+
+        <div class="input-group input mb-3">
+            <span id="spantext" class="marginspan text-white spantext">Subscribe to Our Newsletter</span>
+            <input type="text" class="form-control  margin " placeholder="Your Email Here" aria-label="Recipient's username"
+                aria-describedby="basic-addon2">
+            <span class="input-group-text widthspan margin text-uppercase" id="addon-wrapping">@</span>
+        </div>
+
+
+
+
+
     </div>
     <div class="my_bg">
         <div class="container position-relative ">
@@ -29,8 +41,46 @@ export default {
 </template>
 
 <style lang="scss" scoped>
+#spantext {
+    font-size: 20px;
+    top: 35px;
+
+    font-weight: 700;
+    position: absolute;
+}
+
+.widthspan {
+    width: 45px;
+    height: 50px;
+    aspect-ratio: 1/1;
+    background-color: #ce7c2a;
+    color: white;
+    font-size: 25px;
+    margin-right: 1rem;
+
+
+}
+
+
+// .marginspan {
+// margin-top: 2.5rem;
+// 
+// }
+
+.margin {
+    margin-top: 4.5rem;
+
+
+}
+
 .container {
     height: 100%;
+}
+
+.input {
+    width: 500px;
+    height: 50px;
+    margin-left: 7rem;
 }
 
 .my_bg {
@@ -46,7 +96,7 @@ export default {
     height: 150px;
     margin-top: 5rem;
     position: absolute;
-    margin-left: 36rem;
+    margin-left: 27.5rem;
     z-index: 2;
     background-color: #ce7c2a;
     border-radius: 10px;
